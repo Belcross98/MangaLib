8bf70052bbc59985344f146863c86d825110b41a
Belcross98 <aleksandar.mitkovic1@gmail.com>
2025-03-27 15:14:54 +0100

    Added Login and paritally added authorization with api calls

--- vaporAPI/Controllers/AccountController.cs
+++ vaporAPI/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using vaporAPI.Dtos.Account;
 using vaporAPI.Dtos.User;
 using vaporAPI.Interfaces.Service;
@@ -14,11 +15,48 @@ namespace vaporAPI.Controllers
     {
         private readonly UserManager<User> _userMangager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<User> userManager, ITokenService tokenService)
+        private readonly SignInManager<User> _signInManager;
+        public AccountController(UserManager<User> userManager, ITokenService tokenService, SignInManager<User> signInManager)
         {
             _userMangager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
         }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userMangager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Username not found / password incorrect");
+
+            }
+
+            return Ok(
+
+                new NewUserDto
+                {
+
+                    Username = user.UserName,
+                    Email = user.Email,
+                    Tokens = _tokenService.CreateToken(user)
+                }
+            );
+
+        }
+
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
